4471f4fc43593e213bbddc1fd90b301e84f3d25b
netsec2minutes <53455970+netsec2minutes@users.noreply.github.com>
2023-06-09 00:23:55 -0300

    Update zap_to_sarif.py

--- zap_to_sarif.py
+++ zap_to_sarif.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def convert_zap_json_to_sarif(json_report):
     sarif_report = {
@@ -46,4 +47,14 @@ def convert_zap_json_to_sarif(json_report):
             ]
         })
 
-    return json.dumps(sarif_report)
+    return sarif_report
+
+with open('report.json') as zap_report_file:
+    zap_report = json.load(zap_report_file)
+
+sarif_report = convert_zap_json_to_sarif(zap_report)
+
+with open('./zap_report.sarif', 'w') as sarif_report_file:  # Guarantindo que o relatório SARIF seja salvo na raiz do repositório
+    json.dump(sarif_report, sarif_report_file, indent=4)
+
+print(os.path.abspath('./zap_report.sarif'))  # Imprimindo o caminho absoluto para o arquivo SARIF
